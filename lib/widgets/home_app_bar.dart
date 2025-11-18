ce04ec64e60c1e83a87f0e54368abcec6a0cdd68
Mohanad Abdallah <mohanad1967@yahoo.com>
2025-11-18 13:17:00 +0200

    modifying empname to accept null for now

--- lib/widgets/home_app_bar.dart
+++ lib/widgets/home_app_bar.dart
@@ -170,7 +170,7 @@ class _HomeAppBarState extends State<HomeAppBar> {
 
     final String displayName =
         (isRtl ? widget.user.empName : widget.user.empNameE) ??
-        widget.user.empName;
+        widget.user.empName??'';
     final String displayJob =
         (isRtl ? widget.user.role_name_a : widget.user.role_name_e) ??
         widget.user.role_name_a ??
